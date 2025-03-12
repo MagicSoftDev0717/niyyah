1a2f15a5676c29c1c2bb3bcd69eb4f220e494cee
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-03-13 06:06:39 +0700

    update hero section

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -21,19 +21,19 @@ const Hero = () => {
                 <div className="absolute inset-0 bg-gradient-to-b from-black via-transparent to-black opacity-80"></div>
 
                 {/* Content Section: Positioned at the bottom 1/5 of the screen */}
-                <div className="absolute bottom-1/5 w-full flex flex-col items-center justify-center text-white text-center px-6">
+                <div className="absolute bottom-1/6 w-full flex flex-col items-center justify-center text-white text-center px-6">
                     {/* Responsive Heading */}
                     <h2
                         className="leading-tight font-bold"
                         style={{
-                            fontSize: "clamp(1.5rem, 5vw, 4rem)", // Adjust for mobile and desktop
+                            fontSize: "clamp(2rem, 5vw, 4rem)", // Adjust for mobile and desktop
                             lineHeight: "1.2", // Adjust line height for better spacing
                         }}
                     >
                         <span>
                             Where <span style={{ color: "#1FE4C6" }}>Faith</span> Meets
                         </span>
-                        <br />
+                        <br className="md:inline" />
                         <span>
                             <span style={{ color: "#A18FF7" }}>Innovation</span> in Business
                         </span>
@@ -41,7 +41,7 @@ const Hero = () => {
 
                     {/* CTA Button */}
                     <button
-                        className="mt-6 px-8 md:px-12 py-3 md:py-2 text-white bg-purple-600 font-bold border border-transparent hover:border-white transition-all duration-300"
+                        className="mt-6 px-8 md:px-12 py-3 md:py-3 text-white bg-purple-600 font-bold border border-transparent hover:border-white transition-all duration-300"
                         style={{fontSize: "clamp(1rem, 2vw, 1.5rem)"}}>
                         Let&apos;s Start
                     </button>
