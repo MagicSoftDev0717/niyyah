699a2d5bc2fa01bfec13a370d0195830c84a72d1
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-03-14 14:19:52 +0700

    update footer bg color

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -65,7 +65,7 @@ const Header = () => {
 
 
     return (
-        <header className="fixed top-0 z-50 w-full bg-black text-white shadow-lg">
+        <header className="fixed top-0 z-50 w-full bg-gray-900 text-white shadow-lg">
             <div className="container mx-auto px-4 py-4">
                 {/* Mobile: Two Rows | Desktop: One Row */}
                 {!isMobile ?
@@ -74,15 +74,15 @@ const Header = () => {
                         <div className="flex justify-between w-full md:w-auto gap-6">
 
                             <div className="flex items-center">
-                                <Image src="/assets/Logo/Logo_Niyyah.svg" alt="Logo" width={160} height={80} />
+                                <Image src="/assets/Logo/Logo_Niyyah.svg" alt="Logo" width={180} height={90} />
                             </div>
 
                             <div className="flex items-center">
                                 <Image
                                     src={imageSrc}
                                     alt="icon"
-                                    width={45}
-                                    height={45}
+                                    width={50}
+                                    height={50}
                                     onMouseEnter={() => setImageSrc("/assets/Header/Property_Variant2.svg")}
                                     onMouseLeave={() => setImageSrc("/assets/Header/Property_Default.svg")}
                                 // onClick={() => setIsModalOpen(true)} // Open modal on click
@@ -94,38 +94,39 @@ const Header = () => {
                         {/* Row 2: Date, Other Elements */}
                         <div className="flex justify-between w-full md:w-auto gap-6">
                             <div className="flex items-center space-x-4">
-                                <div className="flex flex-row md:flex-wrap justify-end w-full md:w-auto items-center gap-2 text-center md:text-right">
-                                    <p className="text-sm w-1/2 md:w-full">Ramadan 1, 1446 AH</p>
-                                    <p className="text-sm w-1/2 md:w-full">{`${dayShort}, ${monthName} ${date}, ${year}`}</p>
+                                <div className="flex flex-row md:flex-wrap justify-end w-full md:w-auto items-center text-center md:text-right">
+                                    <p className="text-lg w-1/2 md:w-full">Ramadan 1, 1446 AH</p>
+                                    <p className="text-lg w-1/2 md:w-full">{`${dayShort}, ${monthName} ${date}, ${year}`}</p>
                                 </div>
                             </div>
-                            <div className="flex justify-between w-full md:w-auto gap-6">
-                                <div className="flex items-center space-x-4" onClick={() => setIsOpen(!isOpen)}>
+                            <div className="flex justify-between w-full md:w-auto gap-6 ">
+                                <div className="flex items-center space-x-4" >
                                     <Image
                                         src="/assets/Header/Default_Lang.svg"
                                         alt="Language"
                                         width={24}
                                         height={24}
                                     />
-                                    <span className="text-white hover:text-yellow-500" >{selectedLang}</span>
-                                    <button className="border border-white px-4 py-1">Join Free</button>
+                                    <span className="text-white hover:text-yellow-500 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>{selectedLang}</span>
+                                    {isOpen && (
+                                        <div className="absolute top-full w-40 bg-black text-white shadow-lg">
+                                            {languages.map((lang, index) => (
+                                                <div
+                                                    key={index}
+                                                    className="px-4 py-2 hover:text-black hover:bg-white "
+                                                    onClick={() => {
+                                                        setSelectedLang(lang.code);
+                                                        setIsOpen(false);
+                                                    }}
+                                                >
+                                                    <span className="font-bold"> {lang.code}</span><span>&nbsp;&nbsp;{lang.name} </span>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    )}
+                                    <button className="border text-lg border-white px-6 py-2">JOIN FREE</button>
                                 </div>
-                                {isOpen && (
-                                    <div className="absolute top-full w-40 bg-black text-white shadow-lg">
-                                        {languages.map((lang, index) => (
-                                            <div
-                                                key={index}
-                                                className="px-4 py-2 hover:text-black hover:bg-white cursor-pointer"
-                                                onClick={() => {
-                                                    setSelectedLang(lang.code);
-                                                    setIsOpen(false);
-                                                }}
-                                            >
-                                                <span className="font-bold"> {lang.code}</span><span>&nbsp;&nbsp;{lang.name} </span>
-                                            </div>
-                                        ))}
-                                    </div>
-                                )}
+
                             </div>
 
                         </div>
@@ -154,7 +155,7 @@ const Header = () => {
                                     {languages.map((lang, index) => (
                                         <div
                                             key={index}
-                                            className="px-4 py-2 hover:text-black hover:bg-white cursor-pointer"
+                                            className="px-4 py-2 hover:text-black hover:bg-white"
                                             onClick={() => {
                                                 setSelectedLang(lang.code);
                                                 setIsOpen(false);
