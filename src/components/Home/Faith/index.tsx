6da57620d2f44933f2270b1009a41862d49eb056
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-03-14 12:32:43 +0700

    update faith section

--- src/components/Home/Faith/index.tsx
+++ src/components/Home/Faith/index.tsx
@@ -19,71 +19,130 @@ const AI_Net = () => {
     return (
         <section className="relative w-full h-full flex items-center justify-center overflow-hidden bg-[#001427]">
             <div className={`relative grid ${isMobile ? "" : "grid-rows-12"} w-full mx-auto lg:max-w-full`}>
+                {!isMobile ?
+                    <div className="row-span-2 bg-gradient-to-b from-black to-transparent"
+                        style={{ background: "linear-gradient(180deg, #001427 0%, rgba(0, 20, 39, 0) 100%)" }}>
+                        <div className="grid grid-cols-6 gap-3">
+                            <div className="col-span-1">
 
-                <div className="row-span-2 bg-gradient-to-b from-black to-transparent"
-                    style={{ background: "linear-gradient(180deg, #001427 0%, rgba(0, 20, 39, 0) 100%)" }}>
-                    <div className="grid grid-cols-6 gap-3">
-                        <div className="col-span-1">
-
-                        </div>
-                        <div className="col-span-4">
-                            <div className="items-center justify-center">
-                                <div className="row-span-1 text-white mb-4">
-                                    <div className="flex items-center gap-x-4 mb-8">
-                                        <span className="text-5xl font-bold text-left leading-tight">Faith-Driven Success</span>
+                            </div>
+                            <div className="col-span-4">
+                                <div className="items-center justify-center">
+                                    <div className="row-span-1 text-white mb-4">
+                                        <div className="flex items-center gap-x-4 mb-8">
+                                            <span className="text-5xl font-bold text-left leading-tight">Faith-Driven Success</span>
+                                        </div>
+                                        <span className="text-3xl text-center leading-tight flex flex-col justify-center mb-8">
+                                            Building Business with Purpose
+                                        </span>
+                                        <span className="text-xl text-center leading-tight flex flex-col justify-center">
+                                            Business with intention and barakah starts with the right mindset. Our faith-based insights help you<br />
+                                            align your work with ethical values, purpose, and social impact.
+                                        </span>
                                     </div>
-                                    <span className="text-3xl text-center leading-tight flex flex-col justify-center mb-8">
-                                        Building Business with Purpose
-                                    </span>
-                                    <span className="text-xl text-center leading-tight flex flex-col justify-center">
-                                        Business with intention and barakah starts with the right mindset. Our faith-based insights help you<br />
-                                        align your work with ethical values, purpose, and social impact.
-                                    </span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="row-span-3 grid grid-cols-6 items-center justify-center text-white">
-                    <div></div>
+                    :
 
-                    <div className="flex flex-col items-center gap-4">
-                        <img src="/assets/Faith/Faith1.svg" alt="Image 1" />
-                        <div className="flex flex-col items-center text-center">
-                            <span className="text-lg mb-2" >Honesty & Integrity</span>
-                            <span className="text-medium" >Business built on<br /> truth flourishes</span>
+                    <div
+                        className="row-span-2 bg-gradient-to-b from-black to-transparent flex items-center justify-center text-center mb-4"
+                        style={{ background: "linear-gradient(180deg, #001427 0%, rgba(0, 20, 39, 0) 100%)" }}
+                    >
+                        <div className="w-full max-w-[90%] text-white flex flex-col items-center">
+                            <span className="text-3xl font-bold leading-tight mb-4">Faith-Driven Success</span>
+                            <span className="text-2xl leading-tight mb-4">Building Business with Purpose</span>
+                            <span className="text-lg leading-tight">
+                                Business with intention and barakah starts with the right mindset. Our faith-based insights help you
+                                align your work with ethical values, purpose, and social impact.
+                            </span>
                         </div>
                     </div>
 
-                    <div className="flex flex-col items-center gap-4">
-                        <img src="/assets/Faith/Faith2.svg" alt="Image 4" />
-                        <div className="flex flex-col items-center text-center">
-                            <span className="text-lg mb-2" >Sustainable Growth</span>
-                            <span className="text-medium" >Wealth with<br />responsibility</span>
+
+                }
+                {!isMobile ?
+                    <div className="row-span-3 grid grid-cols-6 items-center justify-center text-white">
+                        <div></div>
+
+                        <div className="flex flex-col items-center gap-4">
+                            <img src="/assets/Faith/Faith1.svg" alt="Image 1" />
+                            <div className="flex flex-col items-center text-center">
+                                <span className="text-lg mb-2" >Honesty & Integrity</span>
+                                <span className="text-medium" >Business built on<br /> truth flourishes</span>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="flex flex-col items-center gap-4">
-                        <img src="/assets/Faith/Faith3.svg" alt="Image 7" />
-                        <div className="flex flex-col items-center text-center">
-                            <span className="text-lg mb-2" >Community Impact</span>
-                            <span className="text-medium" >Profits should<br />empower others</span>
+                        <div className="flex flex-col items-center gap-4">
+                            <img src="/assets/Faith/Faith2.svg" alt="Image 4" />
+                            <div className="flex flex-col items-center text-center">
+                                <span className="text-lg mb-2" >Sustainable Growth</span>
+                                <span className="text-medium" >Wealth with<br />responsibility</span>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="flex flex-col items-center gap-4">
-                        <img src="/assets/Faith/Faith4.svg" alt="Image 7" />
-                        <div className="flex flex-col items-center text-center">
-                            <span className="text-lg mb-2" >Trust & Sincerity</span>
-                            <span className="text-medium" >Barakah follows pure<br /> intentions</span>
+                        <div className="flex flex-col items-center gap-4">
+                            <img src="/assets/Faith/Faith3.svg" alt="Image 7" />
+                            <div className="flex flex-col items-center text-center">
+                                <span className="text-lg mb-2" >Community Impact</span>
+                                <span className="text-medium" >Profits should<br />empower others</span>
+                            </div>
                         </div>
+
+                        <div className="flex flex-col items-center gap-4">
+                            <img src="/assets/Faith/Faith4.svg" alt="Image 7" />
+                            <div className="flex flex-col items-center text-center">
+                                <span className="text-lg mb-2" >Trust & Sincerity</span>
+                                <span className="text-medium" >Barakah follows pure<br /> intentions</span>
+                            </div>
+                        </div>
+                        <div></div>
+
                     </div>
-                    <div></div>
+                    :
+                    <div className="grid grid-cols-2 md:grid-cols-6 gap-4 items-center justify-center text-white mb-4">
+                        <div className="hidden md:block"></div>
 
-                </div>
+                        <div className="flex flex-col items-center gap-4">
+                            <img src="/assets/Faith/Faith1.svg" alt="Image 1" />
+                            <div className="flex flex-col items-center text-center">
+                                <span className="text-lg mb-2">Honesty & Integrity</span>
+                                <span className="text-medium">Business built on<br /> truth flourishes</span>
+                            </div>
+                        </div>
 
-                <div className="row-span-2 items-center justify-center text-white ">
+                        <div className="flex flex-col items-center gap-4">
+                            <img src="/assets/Faith/Faith2.svg" alt="Image 4" />
+                            <div className="flex flex-col items-center text-center">
+                                <span className="text-lg mb-2">Sustainable Growth</span>
+                                <span className="text-medium">Wealth with<br />responsibility</span>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center gap-4">
+                            <img src="/assets/Faith/Faith3.svg" alt="Image 7" />
+                            <div className="flex flex-col items-center text-center">
+                                <span className="text-lg mb-2">Community Impact</span>
+                                <span className="text-medium">Profits should<br />empower others</span>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center gap-4">
+                            <img src="/assets/Faith/Faith4.svg" alt="Image 7" />
+                            <div className="flex flex-col items-center text-center">
+                                <span className="text-lg mb-2">Trust & Sincerity</span>
+                                <span className="text-medium">Barakah follows pure<br /> intentions</span>
+                            </div>
+                        </div>
+
+                        <div className="hidden md:block"></div>
+                    </div>
+                }
+                {!isMobile ? 
+
+                <div className="row-span-2 items-center justify-center text-white w-full max-w-[90%]">
                     <div className="grid grid-cols-9">
                         <div className="col-span-2"></div>
                         <div className="col-span-5">
@@ -101,6 +160,27 @@ const AI_Net = () => {
                         opportunities.
                     </span>
                 </div>
+                :
+                <div className="row-span-2 items-center justify-center text-white ">
+                    <div className="grid grid-cols-9">
+                        <div className="col-span-2"></div>
+                        <div className="col-span-5">
+                            <div className="flex flex-col w-full h-1 bg-white mb-8">
+                            </div>
+                        </div>
+                        <div className="col-span-2"></div>
+                    </div>
+                    <span className="text-2xl text-center leading-tight flex flex-col justify-center mb-8">
+                        Get Clarity, Take Action, and Grow!
+                    </span>
+                    <span className="text-lg text-center leading-tight flex flex-col items-center justify-center mb-16">
+                        Every great entrepreneur faces challenges — but you don’t have to figure it out alone. Answer these
+                        quick questions, and we’ll help you with a personalized plan to overcome obstacles and unlock new
+                        opportunities.
+                    </span>
+                </div>}
+
+
                 <div className="row-span-3 items-center justify-center text-white">
 
                     <span className="text-2xl text-center leading-tight flex flex-col justify-center mb-4">
@@ -170,11 +250,11 @@ const AI_Net = () => {
                     <div className="col-span-2"></div>
 
                 </div>
-          
 
 
 
-       
+
+
             </div >
         </section >
     );
