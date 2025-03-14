f14a6c7d7257cd5c4a9fde0c68322054041d6dac
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-03-14 23:33:42 +0700

    update whole page update

--- src/components/Home/AboutUs/index.tsx
+++ src/components/Home/AboutUs/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { signIn } from "next-auth/react"; // For Google and Apple authentication if using next-auth
+// import { signIn } from "next-auth/react"; // For Google and Apple authentication if using next-auth
 import { FaTimes } from 'react-icons/fa'; // Importing Google and Apple icons
 
 const Hero = () => {
