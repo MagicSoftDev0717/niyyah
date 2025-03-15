cb9c169b1839eb0d2e60f4b2bbbab30ca7b964dd
MagicSoftDev0717 <devsoft0717@gmail.com>
2025-03-15 22:09:35 +0700

    update landing page

--- src/components/Home/Clock/index.tsx
+++ src/components/Home/Clock/index.tsx
@@ -93,9 +93,7 @@ const Clock = () => {
                             <div key={index} className="flex flex-col justify-center items-center text-white text-center">
                                 {/* SVG Container with Relative Positioning */}
                                 <div className={`relative  ${isMobile ? "w-24 h-24" : "w-36 h-36"} flex justify-center items-center`}>
-                                    <img src={`/assets/Clock/${item.img}`} alt={item.label} className="w-3/4 h-3/4" />
-
-                                    {/* Absolutely Centered Time Value */}
+                                    <img src={`/assets/Clock/${item.img}`} alt={item.label} className="w-4/5 h-4/5" />
                                     <span className={`absolute ${isMobile ? "text-6xl" : "text-7xl"} font-bold text-white`}>{item.value}</span>
                                 </div>
 
